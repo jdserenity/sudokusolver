2e8524e7e7fde00d9a82133550f4a00b47853683
J.D <jdcanada00@gmail.com>
2024-08-28 13:16:23 -0400

    got working up to 5 star puzzles in usa today jumbo puzzle book 2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import config, utils
+import config, utils, time
 
-# TODO: 
-# - reimplement "Unique candidate" technique
+# TODO:
 # - implement "Block and column / Row Interaction" technique (only 2 potentials and in the same column or row in the same box)
 # - continue implementing candidate reducing techniques
 # - add error handling codebase wide
+# - make function that prints out the puzzle as it's being solved so it looks like a flipbook animation
+# - add functionality to is_puzzle_solved() that checks if the puzzle was solved correctly so i don't have to squint my eyes and check myself
 
 grid_state = []
 
@@ -35,18 +36,29 @@ def iterative_solve():
     My approach is to create algorithms that model known deductive reasoning techniques, and then iterate through them one by one until I have chipped away at the amount of 'candidates' or possible numbers for each tile until there is only 1. If there is only 1 possible candidate for a tile, then that tile is solved. Using this strategy, I will solve any sudoku puzzle that is used as input.
     """
     
-    global grid_state; is_solved = False; iterations = 0
+    global grid_state; is_solved = False; iterations = 0; start_time = time.time()
 
-    while iterations < 10000 or not is_solved:
+    while iterations < 100 and not is_solved:
         basic_candidate_removal()
         
-        unique_candidate_technique()
-        block_and_column_or_row_interaction_technique()
+        for area in ['box', 'column', 'row']:
+            unique_candidate_technique(area)
+            basic_candidate_removal()
         
         update_solved_entries()
         
         is_solved = utils.is_puzzle_solved(grid_state)
         iterations += 1
+        
+        utils.print_puzzle(grid_state, format='grid_state')
+    
+    end_time = time.time()
+    
+    if is_solved:
+        elapsed_time = end_time - start_time
+        print(f'puzzle solved in {elapsed_time:.6f} seconds\n')
+    else:
+        print('puzzle could not be solved')
 
 
 def basic_candidate_removal():
@@ -68,21 +80,32 @@ def basic_candidate_removal():
                         if comparison_entry[area] == current_entry[area]:
                             current_entry['candidates'].remove(comparison_entry['entry'])
 
-def unique_candidate_technique():
-    global grid_state
 
+def unique_candidate_technique(area):
     # Unique Candidate: If any of the the current box, column, or row have a tile with a candidate that is unique (meaning no other tile in that area has it as a candidate), it means that this unique number is the solved number for that tile.
-
-
-def block_and_column_or_row_interaction_technique():
+    
     global grid_state
 
-    # Block and column / Row Interaction: If in this box there are are only two places a number could possibly appear, AND those two numbers are in the same row or in the same column, that means that that number should be removed from the list of potentials in any tile in the same row or column respectively. (if they are same row, remove from rows, vice versa)
+    for index in range(0, 9):
+        area_candidates = {}
+        area_entries = utils.get_all_entries_from(area, index, grid_state)
+
+        for entry in area_entries:
+            for candidate in entry['candidates']:
+                if (candidate := str(candidate)) not in area_candidates:
+                    area_candidates[candidate] = {'count': 0, 'indices': []}
+                area_candidates[candidate]['count'] += 1
+                area_candidates[candidate]['indices'].append(entry['index'])
+
+        for candidate in area_candidates:
+            if area_candidates[candidate]['count'] == 1:
+                solved_entry = grid_state[area_candidates[candidate]['indices'][0]]
+                utils.update_entry_to_solved(solved_entry, solved_num=candidate)
 
 
 def update_solved_entries():
     """
-    Updates the status of grid entries that have been solved.
+    Iterate over the grid and update the status of grid entries that have been solved.
 
     If an entry has only one candidate left, this function sets that candidate as the final entry value.
     It then marks the entry as solved by clearing the candidates list and updating the solved status.
@@ -91,35 +114,23 @@ def update_solved_entries():
     
     for current_entry in grid_state:
         if len(current_entry['candidates']) == 1:
-            current_entry['entry'] = current_entry['candidates'][0]
-            current_entry['candidates'] = []
-            current_entry['solved'] = True
-    
-
-# def showcase_swordfish_placement():
-#     global grid_state
-#     for entry in grid_state:
-#         if entry['index'] in [10, 15, 33, 35, 46, 48, 66, 71]:
-#             print(entry['entry'])
+            utils.update_entry_to_solved(current_entry, solved_num=current_entry['candidates'][0])
 
 
 def main():
     # Get a puzzle from the database
     with open(config.puzzles_database) as data:
         puzzles = data.readlines()
-        puzzle_raw = puzzles[1].strip()
+        puzzle_raw = puzzles[0].strip()
 
     # Print the unsolved puzzle
     utils.print_puzzle(puzzle_raw, format='raw')
     
     # Populate the grid state array with the puzzle data
     set_initial_grid_state(puzzle_raw)
-
+    
     # Solve the puzzle
     iterative_solve()
-    
-    # Print the solved puzzle
-    utils.print_puzzle(grid_state, format='grid_state')
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -20,3 +20,35 @@ def is_puzzle_solved(grid_state):
         if entry['solved'] == False:
             return False
     return True
+
+
+def get_all_entries_from(area, index, grid_state):
+    # Area should be either 'box', 'column', or 'row'
+    # index should be an integer 0-8
+    
+    desired_entries = []
+
+    for entry in grid_state:
+        if entry[area] == index:
+            desired_entries.append(entry)
+
+    return desired_entries
+
+
+def update_entry_to_solved(entry, solved_num=None):
+    entry['entry'] = solved_num
+    entry['candidates'] = []
+    entry['solved'] = True
+
+
+# def showcase_swordfish_placement():
+#     global grid_state
+#     for entry in grid_state:
+#         if entry['index'] in [10, 15, 33, 35, 46, 48, 66, 71]:
+#             print(entry['entry'])
+
+def block_and_column_or_row_interaction_technique():
+    global grid_state
+
+    # Block and column / Row Interaction: If in this box there are are only two places a number could possibly appear, AND those two numbers are in the same row or in the same column, that means that that number should be removed from the list of potentials in any tile in the same row or column respectively. (if they are same row, remove from rows, vice versa)
+    
